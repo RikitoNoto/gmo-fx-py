3c82bccb40da707d7b1c9559fec9e661aa1f8064
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2024-05-03 22:30:40 +0900

    #19 symbols APIの作成

--- gmo_fx/symbols.py
+++ gmo_fx/symbols.py
@@ -1,4 +1,8 @@
+from dataclasses import dataclass
 from enum import Enum
+from gmo_fx.response import Response as ResponseBase
+from requests import get, Response
+from gmo_fx.urls import BASE_URL_PUBLIC
 
 
 class Symbol(Enum):
@@ -16,3 +20,45 @@ class Symbol(Enum):
     GBP_USD = "GBP_USD"
     AUD_USD = "AUD_USD"
     NZD_USD = "NZD_USD"
+
+
+@dataclass
+class Rule:
+    symbol: Symbol
+    min_open_order_size: int
+    max_order_size: int
+    size_step: int
+    tick_size: float
+
+
+class SymbolsResponse(ResponseBase):
+    rules: list[Rule]
+
+    def __init__(self, response: dict):
+        super().__init__(response)
+        self.rules = []
+
+        data = response["data"]
+        self.rules = [
+            Rule(
+                symbol=Symbol(d["symbol"]),
+                min_open_order_size=int(d["minOpenOrderSize"]),
+                max_order_size=int(d["maxOrderSize"]),
+                size_step=int(d["sizeStep"]),
+                tick_size=float(d["tickSize"]),
+            )
+            for d in data
+        ]
+
+
+def get_symbols() -> SymbolsResponse:
+    response: Response = get(f"{BASE_URL_PUBLIC}/symbols")
+    if response.status_code == 200:
+        response_json = response.json()
+        return SymbolsResponse(response_json)
+
+    raise RuntimeError(
+        "取引ルールが取得できませんでした。\n"
+        f"status code: {response.status_code}\n"
+        f"response: {response.text}"
+    )
