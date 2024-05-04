2345420cd51dbe1c35dd374b9aa992b932aac0c1
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2024-05-04 23:46:45 +0900

    #24 setup.pyにパッケージの詳細を追加

--- setup.py
+++ setup.py
@@ -1,3 +1,34 @@
 from setuptools import setup, find_packages
+import gmo_fx
 
-setup(name="gmo_fx", packages=find_packages())
+DESCRIPTION = "GMO社のFX API用Pythonライブラリ"
+NAME = "gmo_fx"
+AUTHOR = "Rikito Noto"
+AUTHOR_EMAIL = "rikitonoto@gmail.com"
+URL = "https://github.com/RikitoNoto/gmo-fx-py"
+LICENSE = "MIT"
+DOWNLOAD_URL = "https://github.com/RikitoNoto/gmo-fx-py"
+VERSION = gmo_fx.__version__
+INSTALL_REQUIRES = [
+    "requests>=2.31.0",
+]
+PACKAGES = [
+    "gmo_fx",
+]
+
+with open("README.md", "r") as fp:
+    readme = fp.read()
+
+setup(
+    name=NAME,
+    description=DESCRIPTION,
+    long_description=readme,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    license=LICENSE,
+    download_url=DOWNLOAD_URL,
+    version=VERSION,
+    install_requires=INSTALL_REQUIRES,
+    packages=PACKAGES,
+)
